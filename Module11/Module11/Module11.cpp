38b25557fff230d96f9b0214aa371f287a9a3c08
VladSush <vladislav040320@gmail.com>
2026-03-27 22:03:21 +0300

    Module11

---
+++ Module11/Module11/Module11.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+
+class Employee {
+public:
+    virtual ~Employee() = default;
+};
+
+class Manager : public Employee {
+public:
+    void manageTeam() {
+        std::cout << "I'm managing it!\n";
+    }
+private:
+
+};
+
+class Developer : public Employee {
+public:
+    void writeCode() {
+        std::cout << "I'm developing it!\n";
+    }
+private:
+
+};
+
+std::vector<Employee*> Employees;
+
+void add(Employee* other) {
+    Employees.push_back(other);
+}
+
+void checkEmployees(){
+    for (int i = 0; i < Employees.size(); ++i) {
+        if (auto m = dynamic_cast<Manager*>(Employees[i])) {
+            m->Manager::manageTeam();
+        }
+        else {
+            auto d = dynamic_cast<Developer*>(Employees[i]);
+            d->Developer::writeCode();
+       }
+   }
+}
+
+int main() {
+    std::cout << "Hello World!\n";
+    Manager John;
+    Developer Mike;
+    Developer Smith;
+    Developer Jenna;
+    add(&Smith);
+    add(&John);
+    add(&Mike);
+    add(&Jenna);
+    checkEmployees();
+}
+
