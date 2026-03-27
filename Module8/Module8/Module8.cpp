6e5e781e3bc0c201db1058a3c2f75d359440b4f4
VladSush <vladislav040320@gmail.com>
2026-03-27 21:50:32 +0300

    Module8

--- Module8/Module8/Module8.cpp
+++ Module8/Module8/Module8.cpp
@@ -1,20 +1,63 @@
-﻿// Module8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+class Book
+{public:
+    Book(std::string title,
+        std::string author,
+        std::string year,
+        const char* ISBN) {
+        _title = title;
+        _author = author;
+        _year = year;
+        _ISBN = ISBN;
+    }
+
+    Book(const Book& other)
+        : _title(other._title),
+        _author(other._author),
+        _year(other._year),
+        _ISBN(other._ISBN)
+    {
+        std::cout << "copied\n";
+    }
+
+    ~Book() {
+        std::cout << "deleted\n";
+    }
+
+    std::string getTitle() const {
+        return _title;
+    }
+    std::string getAuthor() const {
+        return _author;
+    }
+    std::string getYear() const {
+        return _year;
+    }
+    std::string getISBN() const {
+        return _ISBN;
+    }
+
+    void printinfo() {
+        std::cout << "Name: " << getTitle()
+            << ", Author: " << getAuthor()
+            << ", Year: " << getYear()
+            << ", Serial number: " << getISBN()
+            << "\n";
+    }
+
+private:
+    std::string _title;
+    std::string _author;
+    std::string _year;
+    const char* _ISBN;
+};
 
 int main()
 {
     std::cout << "Hello World!\n";
+    Book DorianGray("The Picture of Doiran Gray", "Oscar Wilde", "1890", "1234567890123");
+    Book PictureDorian(DorianGray);
+    DorianGray.printinfo();
+    PictureDorian.printinfo();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
